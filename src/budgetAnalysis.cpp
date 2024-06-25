2b170a17a9f908050063332c2ee574b8cc60e215
davidspiv <daspivack@gmail.com>
2024-06-25 13:34:55 -0700

    removed function declarations

--- src/budgetAnalysis.cpp
+++ src/budgetAnalysis.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
 #include <string>
 
-void display(std::string);
-int getInput();
-int getTransactionSum();
-std::string getResultString(int);
-std::string formatUsd(int);
-bool isOverflow(int);
-void endProgram(std::string);
+bool isOverflow(int input) {
+  return input == 2147483647 || input == -2147483648;
+}
 
-int main() {
-  int budgetInput, transactionSum, budgetDiff;
-  std::string resultString;
-
-  std::cout << "Enter this month's budget: ";
-
-  budgetInput = getInput();
-
-  std::cout <<
-      R"(Type an integer and press enter to input a transaction amount.
-Enter as many transactions as you'd like.
-Then enter 0 to initiate the calculation.
-)";
-
-  transactionSum = getTransactionSum();
-  budgetDiff = budgetInput - transactionSum;
-  resultString = getResultString(budgetDiff);
-
-  std::cout << (resultString + "\n");
+void endProgram(std::string err) {
+  std::cout << err + "\n";
+  exit(1);
 }
 
 int getInput() {
@@ -38,6 +18,27 @@ int getInput() {
     endProgram("Budget input outside of integer range");
   };
   return input;
+}
+
+std::string formatUsd(int input) {
+  std::string usdString, hundreds, thousands, millions, usdFormatted;
+
+  usdString = std::to_string(input);
+  millions = thousands = "";
+
+  if (usdString.size() > 6) {
+    millions = usdString.substr(0, usdString.size() - 6) + ",";
+    usdString = usdString.substr(usdString.size() - 6, usdString.size());
+  }
+
+  if (usdString.size() > 3) {
+    thousands = usdString.substr(0, usdString.size() - 3) + ",";
+    usdString = usdString.substr(usdString.size() - 3, usdString.size());
+  }
+
+  hundreds = usdString;
+  usdFormatted = "$" + millions + thousands + hundreds;
+  return usdFormatted;
 }
 
 int getTransactionSum() {
@@ -65,35 +66,26 @@ std::string getResultString(int budgetDiff) {
     resultStringComponent = " over ";
   }
 
-  return "You are " + formatUsd(budgetDiff) + resultStringComponent + "budget.";
+  return "Result: " + formatUsd(budgetDiff) + resultStringComponent + "budget.";
 }
 
-std::string formatUsd(int input) {
-  std::string usdString, hundreds, thousands, millions, usdFormatted;
+int main() {
+  int budgetInput, transactionSum, budgetDiff;
+  std::string resultString;
 
-  usdString = std::to_string(input);
-  millions = thousands = "";
+  std::cout << "Enter this month's budget: ";
 
-  if (usdString.size() > 6) {
-    millions = usdString.substr(0, usdString.size() - 6) + ",";
-    usdString = usdString.substr(usdString.size() - 6, usdString.size());
-  }
+  budgetInput = getInput();
 
-  if (usdString.size() > 3) {
-    thousands = usdString.substr(0, usdString.size() - 3) + ",";
-    usdString = usdString.substr(usdString.size() - 3, usdString.size());
-  }
+  std::cout <<
+      R"(Type an integer and press enter to input a transaction amount.
+Enter as many transactions as you'd like.
+Then enter 0 to initiate the calculation.
+)";
 
-  hundreds = usdString;
-  usdFormatted = "$" + millions + thousands + hundreds;
-  return usdFormatted;
-}
+  transactionSum = getTransactionSum();
+  budgetDiff = budgetInput - transactionSum;
+  resultString = getResultString(budgetDiff);
 
-bool isOverflow(int input) {
-  return input == 2147483647 || input == -2147483648;
-}
-
-void endProgram(std::string err) {
-  std::cout << err + "\n";
-  exit(1);
+  std::cout << (resultString + "\n");
 }
