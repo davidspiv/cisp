6b5a7dd8fcedb7e3d19dec0e591d1401e3a566f7
davidspiv <daspivack@gmail.com>
2024-06-27 14:05:52 -0700

    sequentialSearch

--- src/searchBenchmarks.cpp
+++ src/searchBenchmarks.cpp
@@ -1,14 +1,21 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 
 void display(std::string, bool = false);
 int* popTestArr(int*, int);
-std::string buildArrStr(int[], int);
+int sequentialSearch(int*, int);
+int binarySearch(int*, int);
+// std::string buildArrStr(int[], int);
 
 int main() {
-  int testArr[10];
-  popTestArr(testArr, 10);
-  display(buildArrStr(testArr, 10), 1);
+  const int arrSize = 10;
+  int testArr[arrSize];
+  popTestArr(testArr, arrSize);
+  display(std::to_string(sequentialSearch(testArr, arrSize)), 1);
+  std::sort(testArr, testArr + arrSize);
+  display(std::to_string(binarySearch(testArr, arrSize)), 1);
+  // display(buildArrStr(testArr, arrSize), 1);
 }
 
 void display(std::string output, bool returnFlag) {
@@ -17,19 +24,38 @@ void display(std::string output, bool returnFlag) {
 }
 
 int* popTestArr(int* arr, int size) {
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < size; i++) {
     int randomNum = rand();
     arr[i] = randomNum;
   }
+  arr[5] = 69;
   return arr;
 }
 
-std::string buildArrStr(int arr[], int size) {
-  std::string resultString;
-
+int sequentialSearch(int* arr, int size) {
+  int count = 0;
   for (int i = 0; i < size; i++) {
-    resultString += " " + std::to_string(arr[i]) + ",\n";
-  }
-
-  return resultString;
+    count++;
+    if (arr[i] == 69) return count;
+  };
+  return size;
 }
+
+int binarySearch(int* arr, int size) {
+  int count = 0;
+  while (int i = 0; i < size; i++) {
+    count++;
+    if (arr[i] == 69) return count;
+  };
+  return size;
+}
+
+// std::string buildArrStr(int arr[], int size) {
+//   std::string resultString;
+
+//   for (int i = 0; i < size; i++) {
+//     resultString += " " + std::to_string(arr[i]) + ",\n";
+//   }
+
+//   return resultString;
+// }
