3bad59205e09b9f928d259a3d9fc4494d6cdf0ce
davidspiv <daspivack@gmail.com>
2024-07-23 15:46:35 -0700

    reformatted isWinningMove

--- src/ticTacToe.cpp
+++ src/ticTacToe.cpp
@@ -3,17 +3,20 @@
 
 using String = std::string;
 
-struct Tile {
+struct Cord {
   int row;
   int col;
 };
 
-void display(String output, bool returnFlag = false) {
-  if (returnFlag) output += "\n";
-  std::cout << output;
+void display(const String &output, bool returnFlag = false) {
+  if (returnFlag) {
+    std::cout << output << std::endl;
+  } else {
+    std::cout << output;
+  };
 }
 
-int getInput(int minVal, int maxVal) {
+int getIntegerInput(int minVal, int maxVal) {
   int input;
 
   std::cin >> input;
@@ -31,98 +34,90 @@ int getInput(int minVal, int maxVal) {
 }
 
 void displayBoard(char boardArr[3][3]) {
-  for (int i = 0; i < 3; i++) {
+  // three horizontal "slices" per row
+  String outputString = "";
+  for (int row = 0; row < 3; row++) {
     const String topSlice = "     /     /     ";
-    const String bottomSlice = (i == 2) ? topSlice : "_____/_____/_____";
+    const String bottomSlice = (row != 2) ? "_____/_____/_____" : topSlice;
     String middleSlice = "";
-    for (int j = 0; j < 3; j++) {
-      middleSlice += boardArr[i][j] ? "  " + String({boardArr[i][j]}) : "   ";
-      if (j < 2) {
+
+    for (int col = 0; col < 3; col++) {
+      middleSlice +=
+          boardArr[row][col] ? "  " + String({boardArr[row][col]}) : "   ";
+      if (col != 2) {
         middleSlice += "  /";
       }
     }
-
-    display(topSlice, true);
-    display(middleSlice, true);
-    display(bottomSlice, true);
+    outputString += topSlice + "\n" + middleSlice + "\n" + bottomSlice + "\n";
   }
+  display(outputString);
 }
 
-Tile getTile() {
+Cord getCord() {
   display("row: ");
-  const int playerRow = getInput(1, 3);
+  const int playerRow = getIntegerInput(1, 3);
   display("col: ");
-  const int playerCol = getInput(1, 3);
-  Tile newTile = {playerRow - 1, playerCol - 1};
-  return newTile;
+  const int playerCol = getIntegerInput(1, 3);
+  return {playerRow - 1, playerCol - 1};
 }
 
-Tile playerMove(char boardArr[3][3], char player) {
-  Tile playerTile;
+Cord playerMove(char boardArr[3][3], char player) {
+  Cord targetCord;
   display("Player " + String(1, player) + "'s turn to move.", true);
-  playerTile = getTile();
-  while (boardArr[playerTile.row][playerTile.col] != 0) {
-    display("Tile already occupied, try again.", true);
-    playerTile = getTile();
+  targetCord = getCord();
+  while (boardArr[targetCord.row][targetCord.col] != 0) {
+    display("Square already occupied, try again.", true);
+    targetCord = getCord();
   }
 
-  boardArr[playerTile.row][playerTile.col] = player;
-  return playerTile;
+  boardArr[targetCord.row][targetCord.col] = player;
+  return targetCord;
 }
 
-bool isWinningMove(char boardArr[3][3], Tile tile) {
-  const char player = boardArr[tile.row][tile.col];
-  String rowCheck = "";
-  String colCheck = "";
-  String diagCheck1 = "";
-  String diagCheck2 = "";
-  String winCode = {player, player, player};
-  bool diagCheckRequired = (tile.row + tile.col) % 2 == 0;
+bool isWinningMove(char boardArr[3][3], Cord cord) {
+  const char player = boardArr[cord.row][cord.col];
+  const bool diagCheckRequired = (cord.row + cord.col) % 2 == 0;
+  bool rowCheck, colCheck, diagCheck1, diagCheck2;
+  rowCheck = colCheck = diagCheck1 = diagCheck2 = true;
 
-  // change to do while??
   for (int i = 0; i < 3; i++) {
-    rowCheck += boardArr[tile.row][i];
-    colCheck += boardArr[i][tile.col];
-    if (diagCheckRequired) {
-      diagCheck1 += boardArr[i][i];
-      diagCheck2 += boardArr[i][2 - i];
+    if (rowCheck && boardArr[cord.row][i] != player) rowCheck = false;
+    if (colCheck && boardArr[i][cord.col] != player) colCheck = false;
+    if (!diagCheckRequired) {
+      diagCheck1 = diagCheck2 = false;
+    } else {
+      if (diagCheck1 && boardArr[i][i] != player) diagCheck1 = false;
+      if (diagCheck2 && boardArr[i][2 - i] != player) diagCheck2 = false;
     }
   }
 
-  if (rowCheck == winCode || colCheck == winCode || diagCheck1 == winCode ||
-      diagCheck2 == winCode) {
-    return true;
-  }
-  return false;
+  return rowCheck || colCheck || diagCheck1 || diagCheck2;
 }
 
 int main() {
-  bool isProgramEnd = false;
-  while (!isProgramEnd) {
+  do {
     char board[3][3] = {{}};
 
     displayBoard(board);
 
-    for (int i = 0; i < 9; i++) {
-      const char player = i % 2 ? 'X' : 'O';
-      const Tile lastPlayedTile = playerMove(board, player);
+    for (int moveCount = 0; moveCount < 9; moveCount++) {
+      const char player = moveCount % 2 ? 'X' : 'O';
+      const Cord lastPlayedCord = playerMove(board, player);
 
       displayBoard(board);
 
-      if (isWinningMove(board, lastPlayedTile)) {
+      if (isWinningMove(board, lastPlayedCord)) {
         display("Game over: Player " + String({player}) + " wins!!", true);
         break;
       }
-      if (i == 8) {
+      if (moveCount == 8) {
         display("Game over: Tie!!", true);
       }
     }
 
-    display("Would you like to play another game? 0 = no, 1 = yes", true);
-    int input = getInput(0, 1);
-    if (!input) {
-      isProgramEnd = true;
-    }
-  }
-  display("Program exited successfully", 1);
+    display("Would you like to play another game? 0 = no, 1 = yes: ");
+
+  } while (getIntegerInput(0, 1));
+
+  display("Program exited successfully", true);
 }
