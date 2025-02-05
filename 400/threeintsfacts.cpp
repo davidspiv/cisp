2c411315a5d6ac1072853bb1e71dd49055ea8939
davidspiv <daspivack@gmail.com>
2025-02-04 21:19:05 -0800

    pre_rsa

--- 400/threeintsfacts.cpp
+++
@@ -1,9 +0,0 @@
-int ThreeIntsSum(int num1, int num2, int num3) {
-   return (num1 + num2 + num3);
-}
-
-int ThreeIntsAvg(int num1, int num2, int num3) {
-   int sum;
-   sum = num1 + num2 + num3;
-   return (sum / 3);
-}
--- 400/threeintsfacts.h
+++
@@ -1,2 +0,0 @@
-int ThreeIntsSum(int num1, int num2, int num3);
-int ThreeIntsAvg(int num1, int num2, int num3);
