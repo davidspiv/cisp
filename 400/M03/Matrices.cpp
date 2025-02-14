5cbdd397948b3a297b6c2396110b072129de4a16
davidspiv <daspivack@gmail.com>
2025-02-14 14:17:13 -0800

    pre * operator

--- 400/M03/Matrices.cpp
+++ 400/M03/Matrices.cpp
@@ -1,12 +1,21 @@
 #include "Matrices.h"
 
+#include <string>
+
 namespace Matrices {
 /// Add each corresponding element.
 /// usage:  c = a + b;
 Matrix operator+(const Matrix& a, const Matrix& b) {
-  if (a.getRows() == b.getRows() && a.getCols() == b.getCols()) {
-    Matrix matrix(3, 3);
-	
+  int numRows = a.getRows();
+  int numCols = a.getCols();
+  if (b.getRows() == numRows && b.getCols() == numCols) {
+    Matrix matrix(numRows, numCols);
+    for (int row = 0; row < numRows; row++) {
+      for (int col = 0; col < numCols; col++) {
+        matrix(row, col) = a(row, col) + b(row, col);
+      }
+    }
+
     return matrix;
   }
   throw runtime_error("Error: dimensions must agree");
@@ -18,13 +27,37 @@ Matrix operator*(const Matrix& a, const Matrix& b);
 
 /// Matrix comparison.  See description.
 /// usage:  a == b
-bool operator==(const Matrix& a, const Matrix& b);
+bool operator==(const Matrix& a, const Matrix& b) {
+  int numRows = a.getRows();
+  int numCols = a.getCols();
+  if (b.getRows() != numRows && b.getCols() != numCols) {
+    return false;
+  }
+
+  for (int row = 0; row < numRows; row++) {
+    for (int col = 0; col < numCols; col++) {
+      if (a(row, col) != b(row, col)) {
+        return false;
+      };
+    }
+  }
+
+  return true;
+};
 
 /// Matrix comparison.  See description.
 /// usage:  a != b
-bool operator!=(const Matrix& a, const Matrix& b);
+bool operator!=(const Matrix& a, const Matrix& b) { return !(a == b); };
 
 /// Output matrix.
 /// Separate columns by ' ' and rows by '\n'
-ostream& operator<<(ostream& os, const Matrix& a);
+ostream& operator<<(ostream& os, const Matrix& a) {
+  for (int row = 0; row < a.getRows(); row++) {
+    for (int col = 0; col < a.getCols(); col++) {
+      os << setw(10) << a(row, col) << ' ';
+    }
+    os << '\n';
+  }
+  return os;
+};
 }  // namespace Matrices
--- 400/M03/main.cpp
+++ 400/M03/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <string>
 
 #include "Matrices.h"
 #include "io.h"
 int main() {
   // TEST
-  Matrices::Matrix matrix(3, 3);
-  for (int row = 0; row < matrix.getRows(); row++) {
-    for (int col = 0; col < matrix.getCols(); col++) {
-      print(matrix(row, col));
-    }
-  }
+  Matrices::Matrix matrixA(2, 2);
+  Matrices::Matrix matrixB(2, 4);
+
+  matrixB(0, 0) = 1;
+  matrixB(0, 1) = 0.866025;
+  matrixB(0, 2) = 1;
+  matrixB(0, 3) = 0.5;
+  matrixB(1, 0) = 0;
+  matrixB(1, 1) = 0.5;
+  matrixB(1, 2) = 1;
+  matrixB(1, 3) = 0.866025;
+  std::cout << "a:" << std::endl;
+  std::cout << matrixA;
+  std::cout << "b:" << std::endl;
+  std::cout << matrixB;
+  std::cout << "c = a + b:" << std::endl;
+  std::cout << matrixB + matrixB;
+  //   std::cout << (matrixA != matrixB) << std::endl;
 }
