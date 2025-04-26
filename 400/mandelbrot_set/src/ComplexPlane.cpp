e1dd2a5958ba16d54c07634fc40065ce854ad883
davidspiv <daspivack@gmail.com>
2025-04-25 17:10:02 -0700

    updated updateRender

--- 400/mandelbrot_set/src/ComplexPlane.cpp
+++ 400/mandelbrot_set/src/ComplexPlane.cpp
@@ -2,16 +2,54 @@
 
 #include <SFML/Graphics.hpp>
 
-ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) {};
-void ComplexPlane::draw(sf::RenderTarget& target,
-                        sf::RenderStates states) const {};
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+    : m_vArray(
+          sf::VertexArray(sf::PrimitiveType::Points, pixelWidth * pixelHeight)),
+      m_pixel_size(sf::Vector2i(pixelWidth, pixelHeight)),
+      m_aspectRatio((double)pixelWidth / (double)pixelHeight),
+      m_plane_center(sf::Vector2f(0.f, 0.f)),
+      m_plane_size(BASE_WIDTH, BASE_HEIGHT * m_aspectRatio), m_zoomCount(0),
+      m_state(State::CALCULATING) {};
+
+
+void ComplexPlane::draw(sf::RenderTarget &target,
+                        sf::RenderStates states) const {
+  target.draw(m_vArray);
+};
+
+
 void ComplexPlane::zoomln() {};
+
+
 void ComplexPlane::zoomOut() {};
+
+
 void ComplexPlane::setCenter(sf::Vector2i mousePixel) {};
+
+
 void ComplexPlane::setMouseLocation(sf::Vector2i mousePixel) {};
-void ComplexPlane::loadText(sf::Text& text) {};
-void ComplexPlane::updateRender() {};
+
+
+void ComplexPlane::loadText(sf::Text &text) {};
+
+
+void ComplexPlane::updateRender() {
+  const size_t screenWidth = m_plane_size.x;
+  const size_t screenHeight = m_plane_size.y;
+
+  for (size_t j = 0; j < screenHeight; j++) {
+    for (size_t i = 0; i < screenWidth; i++) {
+      m_vArray[j + i * screenWidth].position = {(float)j, (float)i};
+    }
+  }
+};
+
+
 int ComplexPlane::countIterations(sf::Vector2f coord) {};
-void ComplexPlane::iterationsToRGB(size_t count, u_int8_t& r, u_int8_t& g,
-                                   u_int8_t& b) {};
+
+
+void ComplexPlane::iterationsToRGB(size_t count, u_int8_t &r, u_int8_t &g,
+                                   u_int8_t &b) {};
+
+
 sf::Vector2f ComplexPlane::mapPixelToCoords(sf::Vector2i mousePixel) {};
